49fd2edd185b9623afcebc6cb6a598d0b841d742
Obinna Jetbreed Nwasike <obi.java.net@gmail.com>
2025-07-30 15:53:08 +0100

    Lambda update

--- app/src/main/java/com/jetbreed/broadcastreceiver/AirplaneModeChangeReceiver.java
+++ app/src/main/java/com/jetbreed/broadcastreceiver/AirplaneModeChangeReceiver.java
@@ -13,21 +13,18 @@ public class AirplaneModeChangeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Handler handler = new Handler(Looper.getMainLooper());  // Use Looper.getMainLooper() for main thread
-        long delayMillis = 10; // Delay in milliseconds (e.g., 2000ms = 2 seconds)
+        long delayMillis = 10000; // Delay in milliseconds (e.g., 2000ms = 2 seconds)
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (isAirplaneModeOn(
-                        context.getApplicationContext())) {
-                    Toast.makeText(
-                        context, "AirPlane mode is on",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(
-                        context, "AirPlane mode is off",
-                            Toast.LENGTH_LONG).show();
-                }
+        handler.postDelayed(() -> {
+            if (isAirplaneModeOn(
+                    context.getApplicationContext())) {
+                Toast.makeText(
+                    context, "AirPlane mode is on",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(
+                    context, "AirPlane mode is off",
+                        Toast.LENGTH_LONG).show();
             }
         }, delayMillis); // Toast will be shown after the specified delay
     }
